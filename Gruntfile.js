0d24a35202880184e3d59f3765b2a5a538d47398
Philipp Waldmann <philipp.waldmann@s-team.at>
2014-08-18 15:13:16 +0200

    init

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+/*
+ * grunt-deployment
+ * 
+ *
+ * Copyright (c) 2014 Philipp Waldmann
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  process.env.NODE_ENV = 'test';
+
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  var base = process.cwd();
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        base + '/gruntfile.js',
+        base + '/test/**/*.js',
+        base + '/app/**/*.js',
+        base + '/config/**/*.js',
+      ],
+      options: {
+        jshintrc: base + '/.jshintrc'
+      }
+    },
+  
+    mochaTest: {
+      test: {
+        src: 'test/*',
+        options: {
+          reporter: 'spec',
+          recursive: true
+        },
+      },
+    
+      coverage: {
+        src: 'test/*',
+        options: {
+          recursive: true,
+          require: 'blanket'
+        },
+      }
+    }
+  });
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['jshint:all', 'mochaTest:test', 'mochaTest:coverage']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('ci', ['test']);
+
+};
